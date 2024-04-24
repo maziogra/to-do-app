ec1fe7ab4ec3acd7be33c068db6685f07641c6a3
maziogra <mattiagravellu@gmail.com>
2024-04-24 21:43:48 +0200

    add clear button

--- main.js
+++ main.js
@@ -6,23 +6,23 @@ let cancelButton = document.querySelector("#task_input > button.hidden");
 let temp;
 
 let createTask = () => {
-  saveButton.textContent = "SAVE";
-  cancelButton.classList.add("hidden");
-  if (newTask.value === "") {
-    alert("Task is empty");
-  } else {
-    tasks.push(newTask.value);
-    newTask.value = "";
-    refresh();
-    localStorage.setItem("data", JSON.stringify(tasks));
-  }
+	saveButton.textContent = "SAVE";
+	cancelButton.classList.add("hidden");
+	if (newTask.value === "") {
+		alert("Task is empty");
+	} else {
+		tasks.push(newTask.value);
+		newTask.value = "";
+		refresh();
+		localStorage.setItem("data", JSON.stringify(tasks));
+	}
 };
 
-let refresh = (x) => {
-  d_tasks.innerHTML = "";
-  tasks.map((x, y) => {
-    console.log(x);
-    return (d_tasks.innerHTML += `
+let refresh = () => {
+	d_tasks.innerHTML = "";
+	tasks.map((x, y) => {
+		console.log(x);
+		return (d_tasks.innerHTML += `
 			<div id = "${y}" class = "task">
 				<div class = "task_content">${x}</div>
 				<div class = "options">
@@ -30,31 +30,39 @@ let refresh = (x) => {
 					<a onclick = "modifyTask(this)">MODIFY</a>
 				</div>
 			</div>`);
-  });
+	});
 };
 
 let deleteTask = (x) => {
-  tasks.splice(x.parentElement.parentElement.id, 1);
-  localStorage.setItem("data", JSON.stringify(tasks));
-  refresh();
+	tasks.splice(x.parentElement.parentElement.id, 1);
+	localStorage.setItem("data", JSON.stringify(tasks));
+	refresh();
 };
 
 let modifyTask = (x) => {
-  newTask.value = tasks[x.parentElement.parentElement.id];
-  saveButton.textContent = "MODIFY";
-  cancelButton.classList.remove("hidden");
-  d_tasks.innerHTML = "<h1>CLICK CANCEL TO CONTINUE</h1>";
-  temp = tasks[x.parentElement.parentElement.id];
-  tasks.splice(x.parentElement.parentElement.id, 1);
+	newTask.value = tasks[x.parentElement.parentElement.id];
+	saveButton.textContent = "MODIFY";
+	cancelButton.classList.remove("hidden");
+	d_tasks.innerHTML = "<h1>CLICK CANCEL TO CONTINUE</h1>";
+	temp = tasks[x.parentElement.parentElement.id];
+	tasks.splice(x.parentElement.parentElement.id, 1);
 };
 
 let cancel = () => {
-  saveButton.textContent = "SAVE";
-  newTask.value = "";
-  cancelButton.classList.add("hidden");
-  tasks.push(temp);
-  refresh();
+	saveButton.textContent = "SAVE";
+	newTask.value = "";
+	cancelButton.classList.add("hidden");
+	tasks.push(temp);
+	refresh();
 };
+
+let clearTasks = () => {
+	let check = prompt("Type YES to confirm");
+	if(check === "YES"){
+		tasks = [];
+		refresh();
+	}
+}
 
 tasks = JSON.parse(localStorage.getItem("data")) || [];
 refresh();
